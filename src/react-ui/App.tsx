58315fa75c18b590060f5f6c5e232039aeefaaa8
DiegoGonzalezElias <diegogonzalezelias@gmail.com>
2024-09-18 21:09:14 +0200

    feat: added new default navbar component and precreated basic components from shadcn

--- src/react-ui/App.tsx
+++ src/react-ui/App.tsx
@@ -1,12 +1,13 @@
 import './App.css'
-import Map  from './components/Map'
+import Map from './components/Map'
+import NavBar from './components/Navbar'
 
 function App() {
 
   return (
     <div className="flex flex-col h-full w-full max-h-[100vh] relative bg-slate-600">
-      <header className="bg-blue-500 text-white p-4 text-center text-xl absolute top-0 z-50 w-full">
-        Travel Planner Map
+      <header className="left-1/2 transform -translate-x-1/2 max-w-[600px] w-[90%] mx-auto text-white p-4 text-center text-xl absolute top-5 z-50">
+        <NavBar title='Navbar' />
       </header>
       <main className="flex-grow absolute z-10 top-0 w-full">
         <Map />
