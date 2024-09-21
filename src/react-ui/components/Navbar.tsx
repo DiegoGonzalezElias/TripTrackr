d21c9545f4173fd0c364d75f685f0af1966eff92
DiegoGonzalezElias <diegogonzalezelias@gmail.com>
2024-09-21 19:56:57 +0200

    feat: modified components to use i18next

--- src/react-ui/components/Navbar.tsx
+++ src/react-ui/components/Navbar.tsx
@@ -1,3 +1,4 @@
+import { useTranslation } from 'react-i18next';
 import { Button } from './button';
 
 interface NavbarProps {
@@ -5,12 +6,13 @@ interface NavbarProps {
 }
 
 function NavBar({ title }: NavbarProps) {
+    const { t } = useTranslation();
 
 
     return (
         <div className='flex gap-4 bg-slate-700 py-4 px-8 justify-between rounded-lg bg-opacity-45'>
             {title && <h1>{title}</h1>}
-            <Button> Example Button </Button>
+            <Button>{t('BUTTONS.LOGOUT')}</Button>
         </div>
     );
 }
