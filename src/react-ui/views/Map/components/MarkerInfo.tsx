295c352b477021997f7727b9931101cbea3d4e37
DiegoGonzalezElias <diegogonzalezelias@gmail.com>
2025-01-30 10:38:02 +0100

    style: modified style on marker info modal

--- src/react-ui/views/Map/components/MarkerInfo.tsx
+++ src/react-ui/views/Map/components/MarkerInfo.tsx
@@ -25,23 +25,25 @@ function MarkerInfo({ marker }: MarkerInfoProps) {
     return (
         <Card className="w-full max-w-[320px] border-none shadow-none">
             <CardHeader className='flex flex-row justify-between text-start'>
-                <div>
-                    <CardTitle className=' text-lg'>{marker.name}</CardTitle>
-                    <CardDescription>{marker.description}</CardDescription>
+                <div className='max-w-[240px]'>
+                    <CardTitle className='text-lg break-words overflow-hidden'>
+                        <span title={marker.name} className="line-clamp-2">{marker.name}</span>
+                    </CardTitle>
+                    {marker.description && <CardDescription className='text-gray-800 break-words overflow-hidden'>
+                        <span>{marker.description}</span>
+                    </CardDescription>}
                 </div>
             </CardHeader>
             <CardContent>
                 <form className='text-start'>
-
-
                     {/* Categoría del marcador */}
                     <div className="mb-4 flex items-center gap-4">
-                        <label className="text-gray-800 text-sm">{t('LABELS.CATEGORY') + ': '}</label>
-                        <p className="text-gray-600 text-sm">{marker.category}</p>
+                        <label className="text-gray-600 text-sm">{t('LABELS.CATEGORY') + ': '}</label>
+                        <p className="text-gray-800 text-sm">{marker.category}</p>
                     </div>
                     {/* Fecha */}
                     {marker.date && <div className='mb-4'>
-                        <label className="text-gray-800 text-sm">{t('LABELS.DATE')}</label>
+                        <label className="text-gray-600 text-sm">{t('LABELS.DATE')}</label>
                         <div className='mt-[6px]'>
                             <Calendar
                                 mode="single"
@@ -51,7 +53,7 @@ function MarkerInfo({ marker }: MarkerInfoProps) {
                         </div>
                     </div>}
 
-                    <CardFooter className="flex p-0 mt-10 py-4">
+                    <CardFooter className="flex p-0 py-4">
                         <Button disabled={false} type='button' size={'lg'} className='w-full bg-destructive' onClick={() => { handleDeteleMarker() }}>{t('BUTTONS.DELETE_MARKER')}</Button>
                     </CardFooter>
                 </form>
